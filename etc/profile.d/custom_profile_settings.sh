1ea204c0061b0cae3f9374772b106d7e878ca50a
fkol-k4 <fkol-k4@users.noreply.github.com>
2014-07-30 20:04:47 +0300

    Create custom_profile_settings.sh

---
+++ etc/profile.d/custom_profile_settings.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#   Filename    :   custom_profile_settings.sh
+#   Description :   Sets various global profile settings
+
+
+#   1. Add '/usr/local/bin/custom' directory to PATH ** if it exists **
+[[ -d /usr/local/bin/custom ]] && PATH="$PATH:/usr/local/bin/custom"
