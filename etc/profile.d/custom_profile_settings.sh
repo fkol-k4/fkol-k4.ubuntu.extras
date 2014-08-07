f066be3523545af611f8b09ff083e5d73f69a2df
fkol-k4 <fkolyvas@gmail.com>
2014-08-07 06:40:56 +0300

    new file:   etc/default/apport 	new file:   etc/grub.d/06_custom_theme 	new file:   etc/nanorc 	modified:   etc/profile.d/custom_profile_settings.sh 	new file:   etc/skel/.Xresources 	new file:   etc/skel/.bashrc 	new file:   etc/skel/.config/autostart/custom_disable-DPMS.desktop 	new file:   etc/skel/.local/share/applications/custom-google-chrome.desktop 	new file:   etc/skel/.local/share/applications/custom_firefox.desktop 	new file:   etc/skel/.local/share/applications/custom_gedit.desktop 	new file:   etc/skel/.local/share/applications/custom_gnome-system-monitor.desktop 	new file:   etc/skel/.local/share/applications/custom_gnome-terminal.desktop 	new file:   etc/skel/.local/share/applications/custom_libreoffice.desktop 	new file:   etc/skel/.profile 	new file:   etc/sysctl.d/40-custom-settings.conf 	new file:   root/.bashrc

--- etc/profile.d/custom_profile_settings.sh
+++ etc/profile.d/custom_profile_settings.sh
@@ -6,3 +6,12 @@
 
 #   1. Add '/usr/local/bin/custom' directory to PATH ** if it exists **
 [[ -d /usr/local/bin/custom ]] && PATH="$PATH:/usr/local/bin/custom"
+
+#   2. Enable bash completion in interactive shells
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
