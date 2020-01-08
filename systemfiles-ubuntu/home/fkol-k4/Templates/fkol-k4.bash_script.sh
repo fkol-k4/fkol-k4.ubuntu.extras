87e1120c46cd1b1dae4ba60a0d3d6e478a28dffe
fkol-k4 <fkolyvas@gmail.com>
2020-01-08 13:59:59 +0100

    new file:   systemfiles-ubuntu/home/fkol-k4/Templates/ERGOline Order.ods new file:   systemfiles-ubuntu/home/fkol-k4/Templates/fkol-k4.bash_script.sh new file:   systemfiles-ubuntu/home/fkol-k4/Templates/fkol-k4.python3_script.py Useful Template Files

---
+++ systemfiles-ubuntu/home/fkol-k4/Templates/fkol-k4.bash_script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#   File: /path/to/$FILE
+#   Type: Bash script
+
+#   <This script does this...>
+
+#   Copyright (C) 2014 fkol-k4 (fkolyvas@gmail.com)
+
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation version 2.
+
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+
+#   You should have received a copy of the GNU General Public License along
+#   with this program; if not, write to the Free Software Foundation, Inc.,
+#   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# 	
